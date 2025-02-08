e2e52a5634f96ff161841c825ae4352876cf495f
vicbocanegra25 <81280150+VicBocanegra25@users.noreply.github.com>
2025-02-08 04:29:47 -0600

    First commit

--- src/main/java/hyperskill/Main.java
+++ src/main/java/hyperskill/Main.java
@@ -1,5 +1,7 @@
 package hyperskill;
 
+import hyperskill.ui.CommandInterface;
+
 public class Main {
     public static void main(String[] args) {
         CommandInterface myInterface = new CommandInterface();
